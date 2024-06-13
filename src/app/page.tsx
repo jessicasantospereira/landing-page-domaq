581c596ddc027cf9e9b9b1dd32845d13421c08c0
jessicasantospereira <jessydfj@gmail.com>
2024-06-13 14:21:45 -0300

    feat: alteração pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Banner from "@/components/banner";
 import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+    <main className="flex flex-col items-center justify-between p-20">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <Banner />
+      </div>
     </main>
   );
 }
