3fa578ce737f70d799300deb8318d3539b6409d5
jessicasantospereira <MRA_JPEREIRA@fastalelo.onmicrosoft.com>
2025-05-20 09:55:40 -0300

    fix: remove comentarios

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -15,17 +15,6 @@ const Header = () => {
         <Navbar />
       </div>
     </header>
-    // <header className="bg-white sticky mx-auto max-w-[960px] top-0 px-4 shadow-md z-50">
-    //   <div className="container mx-auto flex justify-between items-center py-4">
-    //     <div className="flex items-center">
-    //       <Image className="h-12 w-auto mr-4" src={logo} alt="Logo" />
-    //       <h1 className="text-teal-950 font-bold text-2xl">
-    //         Refrigeração e Lavadoras
-    //       </h1>
-    //     </div>
-    //     <Navbar />
-    //   </div>
-    // </header>
   );
 };
 
